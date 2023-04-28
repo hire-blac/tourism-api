8af887285c1e9ec23742697d1ff3672d47a98312
danihire@gmail.com <danihire@gmail.com>
2023-04-28 15:09:58 +0100

    cart

--- controllers/bookingControllers.js
+++ controllers/bookingControllers.js
@@ -46,89 +46,85 @@ module.exports.get_createBooking = async (req, res) => {
 // create a new booking
 module.exports.createBooking = async (req, res)=>{
   const user= req.user;
-  const {activityslug, numOfAdults, numOfChildren, amountPaid, date} = req.body;
+  const {activities, numOfAdults, numOfChildren, amountPaid, date} = req.body;
 
   try {
     // find activity
-    const activity = await Activity.findOne({slug: activityslug});
+    for (const activityslug of activities) {
 
-    // check if user email is verified
-    // if (!user.email_verified) {
-    //   return res.status(401).send({
-    //     message: "Pending Account. Please Verify Your Email!",
-    //   });
-    // }
+      const activity = await Activity.findOne({slug: activityslug});
 
-    // save booking
-    Booking.create({
-      user,
-      activity,
-      numOfAdults,
-      numOfChildren,
-      amountPaid,
-      date
-    }).then(booking => {
-
-      // set email content
-      adminMailOptions.html = `<h3>New Booking</h3>
-      <p>
-      <b>Client: </b>${user.firstname} ${user.lastname}<br>
-      <b>Activity: </b>${activity.activityName}<br>
-      <b>Num. of Adults: </b>${booking.numOfAdults}<br>
-      <b>Num. of Children: </b>${booking.numOfChildren}<br>
-      <b>Amount Pait: </b>${booking.amountPaid}<br>
-      <b>Date: </b>${booking.date}
-      </p>`;
-
-      // set email content
-      clientMailOptions.to = user.email;
-      clientMailOptions.html = `<h3>New Booking</h3>
-      <p>
-      <b>Client: </b>${user.firstname} ${user.lastname}<br>
-      <b>Activity: </b>${activity.activityName}<br>
-      <b>Num. of Adults: </b>${booking.numOfAdults}<br>
-      <b>Num. of Children: </b>${booking.numOfChildren}<br>
-      <b>Amount Pait: </b>${booking.amountPaid}<br>
-      <b>Date: </b>${booking.date}
-      </p>`;
-
-      // send email
-      transporter.sendMail(adminMailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
+      // create booking
+      Booking.create({
+        user,
+        activity,
+        numOfAdults,
+        numOfChildren,
+        amountPaid,
+        date
+      }).then(booking => {
+  
+        // set admin email content
+        adminMailOptions.html = `<h3>New Booking</h3>
+        <p>
+        <b>Client: </b>${user.firstname} ${user.lastname}<br>
+        <b>Activity: </b>${activity.activityName}<br>
+        <b>Num. of Adults: </b>${booking.numOfAdults}<br>
+        <b>Num. of Children: </b>${booking.numOfChildren}<br>
+        <b>Amount Pait: </b>${booking.amountPaid}<br>
+        <b>Date: </b>${booking.date}
+        </p>`;
+  
+        // set client email content
+        clientMailOptions.to = user.email;
+        clientMailOptions.html = `<h3>New Booking</h3>
+        <p>
+        <b>Client: </b>${user.firstname} ${user.lastname}<br>
+        <b>Activity: </b>${activity.activityName}<br>
+        <b>Num. of Adults: </b>${booking.numOfAdults}<br>
+        <b>Num. of Children: </b>${booking.numOfChildren}<br>
+        <b>Amount Pait: </b>${booking.amountPaid}<br>
+        <b>Date: </b>${booking.date}
+        </p>`;
+  
+        // send admin email
+        transporter.sendMail(adminMailOptions, function(error, info){
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('Email sent: ' + info.response);
+          }
+        });
+  
+        // send client email
+        transporter.sendMail(clientMailOptions, function(error, info){
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('Email sent: ' + info.response);
+          }
+        });
+  
+        // set whatsapp message content
+        let message = `New Booking \n********************\nClient: ${user.firstname} ${user.lastname}\nActivity: ${activity.activityName}\nNum. of Adults: ${booking.numOfAdults}\nNum. of Children: ${booking.numOfChildren}\nDate: ${booking.date}`;
+        // send whataspp message
+        let data = getTextMessageInput(process.env.RECIPIENT_WAID, message);
+        
+        sendMessage(data)
+        .then(() => console.log("WhatsApp message sent"))
+        .catch(err=>{
+          console.log(err.response.data);
+        });
+    
+        res.json({booking});
+    
+      }).catch (err=>{
+        res.json(err)
       });
-
-      // send email
-      transporter.sendMail(clientMailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
-
-      // set whatsapp message content
-      let message = `New Booking \n********************\nClient: ${user.firstname} ${user.lastname}\nActivity: ${activity.activityName}\nNum. of Adults: ${booking.numOfAdults}\nNum. of Children: ${booking.numOfChildren}\nDate: ${booking.date}`;
-      // send whataspp message
-      let data = getTextMessageInput(process.env.RECIPIENT_WAID, message);
       
-      sendMessage(data)
-      .then(() => console.log("WhatsApp message sent"))
-      .catch(err=>{
-        console.log(err.response.data);
-      });
+    }
   
-      res.json({booking});
-  
-    }).catch (err=>{
-      res.json(err)
-    });
-
   } catch (error) {
     res.json(error)
   }
-
 }
